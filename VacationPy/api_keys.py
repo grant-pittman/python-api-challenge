612e11167b221a2a4ac1891bd24aa7dec1785bf0
Grant Pittman <grantpittman@Grants-MBP.attlocal.net>
2020-06-18 21:25:11 -0500

    Adding finished project files

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE!"
+
+# Google API Key
+g_key = "AIzaSyB_pOnKSDP_iO2oyhlJD-6A0i07vcq2yEw"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "9ce4256b96172b1a250eb2a36a69adec"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
